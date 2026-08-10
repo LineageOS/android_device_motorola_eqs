Nagendra9Kumar/android_device_motorola_tank

eqs: Define vendor.egistec.hardware.fingerprint
 This is a follow-up to 78133d5 from sm7435-common

Change-Id: I31353d89e03db5f5085c715733451e9c3bbdfcda

BoardConfig.mk
@@ -23,6 +23,7 @@ include device/motorola/sm8475-common/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := eqs
 
 # HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest_eqs.xml
 ODM_MANIFEST_SKUS += dne
 ODM_MANIFEST_DNE_FILES := $(DEVICE_PATH)/manifest_dne.xml
 ODM_MANIFEST_SKUS += dn

LineageOS/android_device_motorola_eqs

BoardConfig.mk
@@ -23,6 +23,7 @@ include device/motorola/sm8475-common/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := eqs
 
 # HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_SKUS += dne
 ODM_MANIFEST_DNE_FILES := $(DEVICE_PATH)/manifest_dne.xml
 ODM_MANIFEST_SKUS += dn